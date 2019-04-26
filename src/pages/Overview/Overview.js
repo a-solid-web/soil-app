361c15002c82834d3ab62cd313c232819ac8c052
doom <denis.ismail@code.berlin>
2019-04-26 17:52:35 +0200

    Create Avatar component

--- src/pages/Overview/Overview.js
+++ src/pages/Overview/Overview.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import RequestCard from '../../components/RequestCard';
+import Avatar from '../../components/Avatar';
 
 const Overview = ({ content }) => {
   return (
     <div>
       <RequestCard content="Hello RequestCard" />
+      <Avatar />
     </div>
   );
 };
