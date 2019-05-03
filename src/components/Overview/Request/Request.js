5cb4d13f1f27a22038debd31940c51faa8c83c09
Desislava Belokonska <desislava.belokonska@code.berlin>
2019-05-03 13:56:02 +0200

    Slim down request to just be a wrapper for RequestCard and RequestAvatar

--- src/components/Overview/Request/Request.js
+++ src/components/Overview/Request/Request.js
@@ -2,27 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Request.module.css';
 
-import AvatarRequest from '../RequestAvatar';
-import RequestCard from '../RequestCard';
-
-const Request = ({ avatarImgUrl, username, requestAction, className }) => (
-  <div className={`${styles.requestTab} ${className}`}>
-    <AvatarRequest avatarImgUrl={avatarImgUrl} username={username} />
-    <RequestCard requestAction={requestAction} />
-  </div>
+const Request = ({ className, children }) => (
+  <div className={`${styles.requestTab} ${className}`}>{children}</div>
 );
 
 Request.propTypes = {
-  avatarImgUrl: PropTypes.string,
-  username: PropTypes.string,
-  requestAction: PropTypes.string,
+  children: PropTypes.node.isRequired,
   className: PropTypes.string,
 };
 
 Request.defaultProps = {
-  avatarImgUrl: '',
-  username: 'John Doe',
-  requestAction: 'Action content goes here',
   className: '',
 };
 
